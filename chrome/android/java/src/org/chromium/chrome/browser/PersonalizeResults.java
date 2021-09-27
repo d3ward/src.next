0bf19ab02e9c3517749aa5f1b01731a485e8318e
Eduar Ursu <eduardursu78@gmail.com>
2021-09-27 14:55:03 +0200

    Update csw_mobile_script

--- chrome/android/java/src/org/chromium/chrome/browser/PersonalizeResults.java
+++ chrome/android/java/src/org/chromium/chrome/browser/PersonalizeResults.java
@@ -18,10 +18,7 @@ public class PersonalizeResults {
        if (tab != null && shouldRewrapText) {
           tab.getWebContents().evaluateJavaScript("(function() { var pendingUpdate=false;function viewportHandler(event){if(pendingUpdate)return;pendingUpdate=true;requestAnimationFrame(()=>{pendingUpdate=false;document.getElementsByTagName('html')[0].style.maxWidth=window.visualViewport.width+'px';var miniLeft=visualViewport.offsetLeft;var miniTop = -(visualViewport.offsetTop + visualViewport.offsetTop * ((window.pageYOffset / window.innerHeight) / 2));document.getElementsByTagName('html')[0].style.transition='0s ease-in-out';if (miniLeft == 0 && miniTop == 0) { document.getElementsByTagName('html')[0].style.transform=''; } else { document.getElementsByTagName('html')[0].style.transform='translate('+miniLeft+'px, '+miniTop+'px) scale(1.0)'; } })}window.visualViewport.addEventListener('resize',viewportHandler);window.visualViewport.addEventListener('scroll', viewportHandler); })();", null);
        }
-       if (tab != null && tab.getUrl().getSpec().startsWith("https://chrome.google.com/webstore")) {
-          tab.getWebContents().evaluateJavaScript("(function() { if (!document.location.href.includes('https://chrome.google.com/webstore')) { return; } " + MAKE_USER_AGENT_WRITABLE + " window.navigator.userAgent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.25 Safari/537.36'; window.addEventListener('load', function() { if (document.location.pathname == '/webstore/unsupported') { document.location = '/webstore/'; } var node = document.createElement('style');    document.body.appendChild(node);    window.addStyleString = function(str) {        node.innerHTML = str;    }; addStyleString('div { visibility: visible !important; } '); var t=document.querySelector('meta[name=\"viewport\"]');t&&(t.content=\"initial-scale=0.1\",t.content=\"width=1200\") }); })();", null);
-       }
-        if (tab != null && ContextUtils.getAppSharedPreferences().getBoolean("cws_mobile_friendly", true) &&tab.getUrl().getSpec().startsWith("https://chrome.google.com/webstore")) {
+       if (tab != null && ContextUtils.getAppSharedPreferences().getBoolean("cws_mobile_friendly", true) && tab.getUrl().getSpec().startsWith("https://chrome.google.com/webstore")) {
           tab.getWebContents().evaluateJavaScript(CWS_MOBILE_SCRIPT, null);
        }
        if (tab != null && tab.getUrl().getSpec().startsWith("https://microsoftedge.microsoft.com/addons")) {
@@ -122,6 +119,12 @@ public class PersonalizeResults {
 
     private static final String CWS_MOBILE_SCRIPT = ""
 +"(function() {"
++" if (!document.location.href.includes('https://chrome.google.com/webstore')) {return;}"
++MAKE_USER_AGENT_WRITABLE
++"window.navigator.userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.25 Safari/537.36';"
++"window.addEventListener('load', function () {"
++"if (document.location.pathname == '/webstore/unsupported') {document.location = '/webstore/';}var node = document.createElement('style');document.body.appendChild(node);window.addStyleString = function (str) {node.innerHTML = str;};"
++"addStyleString('div { visibility: visible !important; } ');var t = document.querySelector('meta[name=\"viewport\"]');t && (t.content = \"initial-scale=0.1\",t.content=\"width=1200\")"
 +"var x = document.getElementsByClassName('O-j-gb');var node = document.createElement('style');"
 +"node.innerHTML ='.a-t-o-k {width:auto;}.a-K-o {padding-top:8px!important;}.h-a-x {text-align: center;}.a-t-o-ea {padding-left:10px}.a-t-o-A {width: calc(100vw - 40px)!important;border-radius: 8px;}.menubtn{float:left;padding:4px}.HWJfBb{height:90vh!important;overflow-y:scroll!important;box-shadow:0 3px 6px rgba(0,0,0,.16),0 3px 6px rgba(0,0,0,.23)!important}.h-e-f-Ra-c{position:fixed!important;top:68px!important;left:0px!important;z-index:10000!important}div .a-P-d-k{margin:5px!important;border-radius:5px;padding:5px;box-shadow:0 3px 6px rgba(0,0,0,.16),0 3px 6px rgba(0,0,0,.23)}.e-f-o{padding-top:50px!important}.e-f-n-Va{margin:0 auto!important}.e-f-b.g-b{margin:0 4px!important;padding:6px 7px!important}.e-f-w-Va,.e-f-yb-w{width:calc(100vw - 80px)!important}.back-to-top{width:100px;height:130px;padding:10px;text-align:center;background:whiteSmoke;font-weight:700;color:#444;text-decoration:none;position:fixed;top:75px;right:40px;display:none;background:url(arrow_up.png) no-repeat 0 20px}.back-to-top:hover{text-decoration:none}.yrk3fc{margin:10px 16px!important}span .h-n-j-Z-ea-aa{margin-top:10px!important}img.hA-Ce-ze-Yf,.C-b-i .i-da-A{width:100vw!important}.e-f-Ib-n,.e-f-Ib-p{display:none}.O-j-Ic-c{display:none}.F-x{width:100%!important}img.a-P-d-A{height:100px!important;width:160px!important}.menubutton{display:inline-block;cursor:pointer}.bar1,.bar2,.bar3{width:25px;height:4px;background-color:#5f6368;margin:3px 0;margin-right:6px;transition:0.4s}.change .bar1{-webkit-transform:rotate(-45deg) translate(-5px,5px);transform:rotate(-45deg) translate(-5px,5px)}.change .bar2{opacity:0}.change .bar3{-webkit-transform:rotate(45deg) translate(-5px,-5px);transform:rotate(45deg) translate(-5px,-5px)}.a-La{display:none}.Je-qe-zd-Ge{width:100vw!important}.F-k,.a-P-d{width:auto!important}.PNF6le{text-transform:none!important}div .a-d.webstore-test-wall-tile.a-d-zc.a-P-d-k{width:160px!important}';"
 +"document.body.appendChild(node);var menuStyle = document.createElement('style');"
@@ -130,7 +133,7 @@ public class PersonalizeResults {
 +"menuBtn.className = 'menubtn';menuBtn.innerHTML= '<div class=\"bar1\"></div><div class=\"bar2\"></div><div class=\"bar3\"></div>';"
 +" menuBtn.onclick=function(){menuBtn.classList.toggle(\"change\"),menuStyle.innerHTML=\".F-n-J{display:\"+(showMenu?\"none\":\"inline-block\")+\"!important;position:fixed!important;left:0px!important;width: 100vw!important;max-width: 450px;background:none!important;z-index: 1149!important;}\",showMenu=!showMenu};"
 +"x[0].insertBefore(menuBtn, x[0].firstChild);"
-+"})();";
++"}); })();";
 
     private static final String MESSENGER_SCRIPT = ""
 +"(function() {"
